158bbeae5ffd2b06ebd200db7152f9cb3171f7f4
omnia <omniacis1@gmail.com>
2025-06-30 21:09:01 +0300

    add assets

---
+++ lib/core/utils/color_helper.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class ColorsHelper {
+  static Color lightBlue = HexColor("98A8B8");
+  static Color orange = HexColor("FF7622");
+  static Color green = HexColor("059C6A");
+  static Color yellow = HexColor("FFD27C");
+  static Color orangeDark = HexColor("F58D1D");
+  static Color lightGrey = HexColor("6B6E82");
+  static Color lightBabyBlue = HexColor("ECF0F4");
+  static Color purple = HexColor("413DFB");
+  static Color pink = HexColor("A03BB1");
+  static Color white = HexColor("FFFFFF");
+  static Color black = HexColor("000000");
+}
---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+abstract class Styles {
+  static final textStyle18 = TextStyle(
+    fontSize: 18.sp,
+    fontWeight: FontWeight.w700,
+    fontFamily: "Sen",
+    decoration: TextDecoration.none,
+  );
+  static final textStyle10 = TextStyle(
+    fontSize: 10.sp,
+    fontWeight: FontWeight.w400,
+    fontFamily: "Sen",
+  );
+  static final textStyle12 = TextStyle(
+    fontSize: 12.sp,
+    fontWeight: FontWeight.w600,
+    fontFamily: "Sen",
+  );
+  static final textStyle13 = TextStyle(
+    fontSize: 13.sp,
+    fontWeight: FontWeight.w600,
+    fontFamily: "Sen",
+  );
+  static final textStyle14 = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w400,
+    fontFamily: "Sen",
+  );
+  static final textStyle16 = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w600,
+    fontFamily: "Sen",
+  );
+
+  static final textStyle22 = TextStyle(
+    fontSize: 22.sp,
+    fontWeight: FontWeight.w600,
+    fontFamily: "Sen",
+  );
+  static final textStyle24 = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeight.w600,
+    fontFamily: "Sen",
+  );
+  static final textStyle33 = TextStyle(
+    fontSize: 33.sp,
+    fontWeight: FontWeight.w600,
+    fontFamily: "Sen",
+  );
+}
