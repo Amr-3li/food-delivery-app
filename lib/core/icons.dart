153f6e40f85b747e02e1d4f6c6a5f5dca1e615ee
omnia <omniacis1@gmail.com>
2025-06-30 22:37:02 +0300

    add icons  to core

---
+++ lib/core/icons.dart
@@ -0,0 +1,80 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  static const String assetsImagesBell = "assets/images/bell.svg";
+
+  static const String assetsImagesCall = "assets/images/Call.svg";
+
+  static const String assetsImagesCar = "assets/images/Car.svg";
+
+  static const String assetsImagesCemara = "assets/images/Cemara.svg";
+
+  static const String assetsImagesCheck = "assets/images/Check.svg";
+
+  static const String assetsImagesCheck1 = "assets/images/check (1).svg";
+
+  static const String assetsImagesCreditCard = "assets/images/credit-card.svg";
+
+  static const String assetsImagesCross = "assets/images/Cross.svg";
+
+  static const String assetsImagesEye = "assets/images/eye.svg";
+
+  static const String assetsImagesFilter = "assets/images/Filter.svg";
+
+  static const String assetsImagesHeart = "assets/images/heart.svg";
+
+  static const String assetsImagesHome = "assets/images/Home.svg";
+
+  static const String assetsImagesIcon = "assets/images/Icon.svg";
+
+  static const String assetsImagesInfo = "assets/images/info.svg";
+
+  static const String assetsImagesLogout = "assets/images/Logout.svg";
+
+  static const String assetsImagesMenu = "assets/images/menu.svg";
+
+  static const String assetsImagesMessage = "assets/images/Message.svg";
+
+  static const String assetsImagesMicOff = "assets/images/mic-off.svg";
+
+  static const String assetsImagesMinus = "assets/images/Minus.svg";
+
+  static const String assetsImagesMore = "assets/images/More.svg";
+
+  static const String assetsImagesPlus = "assets/images/Plus.svg";
+
+  static const String assetsImagesPolygon1 = "assets/images/Polygon 1.svg";
+
+  static const String assetsImagesQuestion = "assets/images/Question.svg";
+
+  static const String assetsImagesSearch = "assets/images/Search.svg";
+
+  static const String assetsImagesSend = "assets/images/Send.svg";
+
+  static const String assetsImagesShare = "assets/images/share.svg";
+
+  static const String assetsImagesSmile = "assets/images/smile.svg";
+
+  static const String assetsImagesStar = "assets/images/Star.svg";
+
+  static const String assetsImagesTag = "assets/images/Tag.svg";
+
+  static const String assetsImagesUnderArrow = "assets/images/Under Arrow.svg";
+
+  /// Assets for assetsImagesUser
+  /// assets/images/User.svg
+  static const String assetsImagesUser = "assets/images/User.svg";
+
+  /// Assets for assetsImagesVector
+  /// assets/images/Vector.svg
+  static const String assetsImagesVector = "assets/images/Vector.svg";
+
+  /// Assets for assetsImagesVolume1
+  /// assets/images/volume-1.svg
+  static const String assetsImagesVolume1 = "assets/images/volume-1.svg";
+
+  /// Assets for assetsImagesWatch
+  /// assets/images/Watch.svg
+  static const String assetsImagesWatch = "assets/images/Watch.svg";
+}
