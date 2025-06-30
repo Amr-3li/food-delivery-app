067b35418e97af0aa2dd097a5d1ec4be4febfd17
omnia <omniacis1@gmail.com>
2025-06-30 21:21:23 +0300

    add custom elevated button

---
+++ lib/core/widgets/custom_elevated_button.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import 'package:restaurant/core/utils/styles.dart';
+
+class CustomElevatedButton extends StatelessWidget {
+  const CustomElevatedButton({
+    super.key,
+    required this.buttonText,
+    this.buttonIcon = const SizedBox(width: .5),
+    required this.onPressedFunction,
+
+    this.alignButton = MainAxisAlignment.center,
+    this.widthButton = double.infinity,
+    this.textColor = Colors.white,
+    required this.buttonColor,
+  });
+  final String buttonText;
+
+  final Color? buttonColor;
+  final Color textColor;
+
+  final double widthButton;
+  final Function()? onPressedFunction;
+  final MainAxisAlignment alignButton;
+  final Widget buttonIcon;
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressedFunction,
+      style: ElevatedButton.styleFrom(
+        minimumSize: Size(widthButton, 55),
+        backgroundColor: buttonColor,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+      ),
+      child: Text(
+        buttonText,
+        style: Styles.textStyle18.copyWith(
+          color: textColor,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
