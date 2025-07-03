89a13cf5db351129433e0c0f35b159e5b91d1b54
ola <olafarag343@gmail.com>
2025-07-03 18:09:00 +0300

    first

---
+++ lib/features/food_categories/presentation/widgets/open_restaurants_section.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class OpenRestaurantsSection extends StatelessWidget {
+  const OpenRestaurantsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 120,
+      decoration: BoxDecoration(
+        color: Colors.grey[400],
+        borderRadius: BorderRadius.circular(12),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:restaurant/core/dependency_injection/service_locator.dart';
 import 'package:restaurant/features/cart/presentation/views/cart.dart';
+import 'package:restaurant/features/food_categories/presentation/views/food_categories_screen.dart';
+import 'package:restaurant/features/restaurant_view/presentation/views/restaurant_view_screen.dart';
+import 'package:restaurant/features/restaurant_view/widgets/filter_sheet.dart';
 import 'package:sizer/sizer.dart';
+import 'package:restaurant/features/search/presentation/views/search_screen.dart';
+import 'package:restaurant/features/food_details/presentation/views/food_details_screen.dart';
 
 void main() {
   setupLocator();
@@ -11,16 +16,21 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return Sizer(
       builder: (context, orientation, deviceType) {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
-          home: const CartView(),
+          home: Scaffold(
+            backgroundColor: Colors.grey[200],
+            body: const SafeArea(
+              child: FilterSheet(),
+            ),
+          ),
         );
       },
     );
   }
 }
+
