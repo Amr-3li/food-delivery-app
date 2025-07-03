ce01a4a7d6a5875e5500c115a27bf39da663ce25
omnia <omniacis1@gmail.com>
2025-07-03 22:40:53 +0300

    delete unused import

--- lib/core/helper/app_router.dart
+++ lib/core/helper/app_router.dart
@@ -1,26 +1,20 @@
 import 'package:go_router/go_router.dart';
-import 'package:restaurant/features/auth/views/login_view.dart';
-import 'package:restaurant/features/auth/views/register_view.dart';
+
 import 'package:restaurant/features/chief_part/home/presentation/view/chif_home_view.dart';
 import 'package:restaurant/features/chief_part/notification/presentation/view/notification.dart';
-import 'package:restaurant/features/menu/presentation/views/edit_profile_view.dart';
+
 import 'package:restaurant/features/menu/presentation/views/menu_view.dart';
-import 'package:restaurant/features/onboarding/views/onboarding_page.dart';
-import 'package:restaurant/features/splash/presentation/views/splash_view.dart';
+
 import 'package:restaurant/features/cart/presentation/views/cart.dart';
 import 'package:restaurant/features/chat/presentation/views/chat_screen.dart';
 import 'package:restaurant/features/chat/presentation/views/list_chat_screen.dart';
 import 'package:restaurant/features/chief_part/chief_menu/presentation/view/chief_menu.dart';
-import 'package:restaurant/features/chief_part/home/presentation/view/chif_home_view.dart';
+
 import 'package:restaurant/features/notification/presentation/view/notification.dart';
 import 'package:restaurant/features/payment/presentaion/view/payment_sucess.dart';
 
 import 'package:restaurant/features/reviews/presentation/views/add_review.dart';
 import 'package:restaurant/features/reviews/presentation/views/reviews.dart';
-
-import '../../features/forget_password/presentation/views/forget_password_view.dart';
-import '../../features/forget_password/presentation/views/vertification_view.dart';
-import '../../features/menu/presentation/views/personal_info_view.dart';
 
 abstract class AppRouter {
   static const kSplashView = '/';
