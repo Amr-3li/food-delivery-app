da203d686446ffe676cc2ee609cd5a2dbd704ed9
omnia <omniacis1@gmail.com>
2025-07-06 16:04:07 +0300

    feat: add withdraw screen

--- lib/features/chief_part/chief_menu/presentation/view/withdraw.dart
+++ lib/features/chief_part/chief_menu/presentation/view/withdraw.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:lottie/lottie.dart';
+
 import 'package:restaurant/core/helper/app_router.dart';
 import 'package:restaurant/core/icons.dart';
 import 'package:restaurant/core/utils/color_helper.dart';
