8fe03ac4c27e0b592f64e1ffbf4d0db063bc98ba
omnia <omniacis1@gmail.com>
2025-07-15 23:13:08 +0300

    feat: enhance CustomCategory and CustomRestaurantInfo widgets to accept dynamic data

--- lib/features/home_user/presentation/views/widgets/custom_restaurants_info.dart
+++ lib/features/home_user/presentation/views/widgets/custom_restaurants_info.dart
@@ -3,12 +3,15 @@ import 'package:restaurant/core/helper/app_responsive.dart';
 import 'package:restaurant/core/icons.dart';
 import 'package:restaurant/core/utils/color_helper.dart';
 import 'package:restaurant/core/utils/styles.dart';
+import 'package:restaurant/features/home_user/data/models/restaurant_model.dart';
 import 'package:restaurant/features/home_user/presentation/views/widgets/custom_icons_and_title.dart'
     show CustomIconsTitl;
 import 'package:restaurant/features/home_user/presentation/views/widgets/custom_network_image.dart';
 
 class CustomRestorantInfo extends StatelessWidget {
-  const CustomRestorantInfo({super.key});
+  final RestaurantModel restaurant;
+
+  const CustomRestorantInfo({super.key, required this.restaurant});
 
   @override
   Widget build(BuildContext context) {
@@ -17,19 +20,22 @@ class CustomRestorantInfo extends StatelessWidget {
       children: [
         CustomNetworkImage(
           imageUrl:
-              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfL0Ahbgn1CWY-UjxICwSCX6QA9XPCLiLmfi3K1SPm15LLC8C056QRuYakxRlbx-jMWj8&usqp=CAU",
+              "https://popmenucloud.com/cdn-cgi/image/width%3D1200%2Cheight%3D1200%2Cfit%3Dscale-down%2Cformat%3Dauto%2Cquality%3D60/evuzicja/678850d5-0715-407f-92dd-499326242d71.jpg",
           width: 327,
           height: 140,
         ),
-        Text("rose garden restaurant", style: Styles.textStyle20),
+        Text(restaurant.name, style: Styles.textStyle20),
         Text(
-          "Burger - Chiken - Riche - Wings",
+          restaurant.description,
           style: Styles.textStyle14.copyWith(color: ColorsHelper.grayWords),
         ),
         SizedBox(height: AppResponsive.height(context, value: 10)),
         Row(
           children: [
-            CustomIconsTitl(title: ' 4:7', iconUrl: AppIcons.iStar),
+            CustomIconsTitl(
+              title: ' ${restaurant.averageRating}',
+              iconUrl: AppIcons.iStar,
+            ),
             SizedBox(width: AppResponsive.width(context, value: 12)),
             CustomIconsTitl(title: ' Free', iconUrl: AppIcons.iCar),
             SizedBox(width: AppResponsive.width(context, value: 12)),
