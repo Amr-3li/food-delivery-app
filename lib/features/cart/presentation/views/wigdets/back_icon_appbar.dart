518388b46d9da16b1c0642b056462b4540f93e0c
Mohamed Hosny <mohammedhosny53@gmail.com>
2025-08-01 10:34:35 +0300

    refactor in cart view

--- lib/features/cart/presentation/views/wigdets/back_icon_appbar.dart
+++ lib/features/cart/presentation/views/wigdets/back_icon_appbar.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:restaurant/core/utils/icons.dart';
 import 'package:svg_flutter/svg.dart';
 
+import '../../../../../core/utils/color_helper.dart';
+
 // ignore: must_be_immutable
 class BackIconAppBar extends StatelessWidget {
   const BackIconAppBar({
@@ -13,12 +15,15 @@ class BackIconAppBar extends StatelessWidget {
   final Color iconColor;
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: CircleAvatar(
-        radius: 15,
-        backgroundColor: Colors.white.withValues(alpha: .3),
-        child: SvgPicture.asset(AppIcons.iIcon),
+    return Padding(
+      padding: const EdgeInsets.only(left: 16),
+      child: GestureDetector(
+        onTap: onTap,
+        child: CircleAvatar(
+          radius: 22,
+          backgroundColor: ColorsHelper.lightBabyBlue,
+          child: SvgPicture.asset(AppIcons.iIcon),
+        ),
       ),
     );
   }
